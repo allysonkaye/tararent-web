d116733ad0dbcd8700cdfaf9138506c834f4ff17
allysonkaye <allysonkayetimpug06@gmail.com>
2024-05-04 22:20:05 +0800

    Initial commit

---
+++ javascript/login.js
@@ -0,0 +1,44 @@
+//LOGINNNN
+ 
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
+ import { getDatabase, get, ref, child } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-database.js";
+   import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js";
+   const firebaseConfig = {
+     apiKey: "AIzaSyBAntVFheC4AyJ5n6BGMvZzbfJtisDGafw",
+     authDomain: "admintararent.firebaseapp.com",
+     databaseURL: "https://admintararent-default-rtdb.asia-southeast1.firebasedatabase.app",
+     projectId: "admintararent",
+     storageBucket: "admintararent.appspot.com",
+     messagingSenderId: "419063988334",
+     appId: "1:419063988334:web:c2e91de332e34e53c6267f",
+     measurementId: "G-619H3PF5LZ"
+   };
+ 
+ 
+   const app = initializeApp(firebaseConfig);
+   const database = getDatabase(app);
+   const auth = getAuth(app);
+   const dbref = ref(database);
+ 
+   const submit = document.getElementById("submit");
+   submit.addEventListener("click", function (event) {
+     event.preventDefault();
+ 
+       const email = document.getElementById("email").value;
+       const password = document.getElementById("password").value;
+ 
+    signInWithEmailAndPassword(auth, email, password)
+     .then((userCredential) => {
+       // Signed up 
+       const user = userCredential.user;
+      
+      //  window.location.href = "index.html";
+       // ...
+     })
+     .catch((error) => {
+       const errorCode = error.code;
+       const errorMessage = error.message;
+       // ..
+       alert(errorMessage)
+     });
+   });
---
+++ javascript/signup.js
@@ -0,0 +1,99 @@
+
+ 
+ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
+import { getDatabase, ref, set } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-database.js";
+  import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js";
+  const firebaseConfig = {
+    apiKey: "AIzaSyBAntVFheC4AyJ5n6BGMvZzbfJtisDGafw",
+    authDomain: "admintararent.firebaseapp.com",
+    databaseURL: "https://admintararent-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "admintararent",
+    storageBucket: "admintararent.appspot.com",
+    messagingSenderId: "419063988334",
+    appId: "1:419063988334:web:c2e91de332e34e53c6267f",
+    measurementId: "G-619H3PF5LZ"
+  };
+
+
+  // const app = initializeApp(firebaseConfig);
+  // const database = getDatabase(app);
+  // const auth = getAuth();
+
+  // const submit = document.getElementById("submit");
+  // submit.addEventListener("click", function (event) {
+  //   event.preventDefault();
+
+  //     const email = document.getElementById("email").value;
+  //     const password = document.getElementById("password").value;
+  //     const fullname = document.getElementById('fullname').value;
+  //     const phonenum = document.getElementById('phonenum').value;
+  //     const  place = document.getElementById('place').value;
+
+  //   createUserWithEmailAndPassword(auth, email, password)
+  //   .then((userCredential) => {
+
+  //     const user = userCredential.user;
+
+  //     set(ref(database, 'user/' + user.id), {
+  //       email : email,
+  //       fullname : fullname,
+  //       phonenum : phonenum,
+  //       place : place,
+  //       // last_login : Date.now()
+  //   })
+
+  //     alert('created account')
+  //     window.location.href = "login.html";
+  //     // ...
+  //   })
+  //   .catch((error) => {
+  //     const errorCode = error.code;
+  //     const errorMessage = error.message;
+  //     // ..
+  //     alert(errorMessage)
+  //   });
+  // });
+
+
+
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+const auth = getAuth();
+
+document.getElementById('submit').addEventListener('click', (e) => {
+    e.preventDefault();
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const fullname = document.getElementById('fullname').value;
+    const phonenum = document.getElementById('phonenum').value;
+    const  place = document.getElementById('place').value;
+
+    createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            // ...
+            set(ref(database, 'user/' + user.uid), {
+              email : email,
+              fullname : fullname,
+              phonenum : phonenum,
+              place : place,
+            }
+
+            )
+            alert('created account');
+            
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // ..
+            alert(errorMessage)
+        });
+}) 
+
+
+
